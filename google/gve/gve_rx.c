894c09718499a27791c368e980447db973bea2d1
Catherine Sullivan <csully@google.com>
2020-10-17 15:41:18 -0700

    gve: check that RX QPL actually gets assigned
    And correctly handle it if it isn't.
    
    Signed-off-by: Catherine Sullivan <csully@google.com>
    GitOrigin-RevId: 7fcd3f55813f4d0c0bb1138011547c4af4fc66ba

--- google/gve/gve_rx.c
+++ google/gve/gve_rx.c
@@ -101,8 +101,14 @@ static int gve_prefill_rx_pages(struct gve_rx_ring *rx)
 	if (!rx->data.page_info)
 		return -ENOMEM;
 
-	if (!rx->data.raw_addressing)
+	if (!rx->data.raw_addressing) {
 		rx->data.qpl = gve_assign_rx_qpl(priv);
+		if (!rx->data.qpl) {
+			kfree(rx->data.page_info);
+			rx->data.page_info = NULL;
+			return -ENOMEM;
+		}
+	}
 	for (i = 0; i < slots; i++) {
 		struct page *page;
 		dma_addr_t addr;
@@ -121,6 +127,8 @@ static int gve_prefill_rx_pages(struct gve_rx_ring *rx)
 					gve_rx_free_buffer(&priv->pdev->dev,
 							 &rx->data.page_info[j],
 							 &rx->data.data_ring[j]);
+				kfree(rx->data.page_info);
+				rx->data.page_info = NULL;
 				return err;
 			}
 		} else {
