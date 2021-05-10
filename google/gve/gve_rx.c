254f1baf133eac97bb4bee3f7869b6df6ea937d5
Jordan Kimbrough <jrkim@google.com>
2021-05-10 11:08:29 -0700

    Update rx buffer recycling for larger page sizes
    Signed-off-by: Jordan Kimbrough <jrkim@google.com>
    GitOrigin-RevId: 0a8d6a41b6db858b588ecc51bbcb5b1c4c202a16

--- google/gve/gve_rx.c
+++ google/gve/gve_rx.c
@@ -304,7 +304,7 @@ static void gve_rx_flip_buffer(struct gve_rx_slot_page_info *page_info,
 }
 
 static bool gve_rx_can_flip_buffers(struct net_device *netdev) {
-#if PAGE_SIZE == 4096
+#if PAGE_SIZE >= 4096
 	/* We can't flip a buffer if we can't fit a packet
 	 * into half a page.
 	 */
@@ -312,7 +312,7 @@ static bool gve_rx_can_flip_buffers(struct net_device *netdev) {
 		return false;
 	return true;
 #else
-	/* PAGE_SIZE != 4096 - don't try to reuse */
+	/* PAGE_SIZE < 4096 - don't try to reuse */
 	return false;
 #endif
 }
