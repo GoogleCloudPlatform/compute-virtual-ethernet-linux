f43ded1aab97aaf202f66151a3a607d567ec0d7e
Rushil Gupta <rushilg@google.com>
2023-08-08 23:20:59 +0000

    gve: Bump up GVE_VERSION to v1.4.0rc3

--- google/gve/gve_main.c
+++ google/gve/gve_main.c
@@ -26,7 +26,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		"1.4.0rc2"
+#define GVE_VERSION		"1.4.0rc3"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
