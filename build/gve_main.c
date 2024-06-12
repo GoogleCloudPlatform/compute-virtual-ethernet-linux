9d957a8d28aed8970a4b6fbce5541413cbac3132
Ziwei Xiao <ziweixiao@google.com>
2024-06-12 00:16:54 +0000

    gve: Clear napi->skb before dev_kfree_skb_any()
    gve_rx_free_skb incorrectly leaves napi->skb referencing an skb after it
    is freed with dev_kfree_skb_any(). This can result in a subsequent call
    to napi_get_frags returning a dangling pointer.
    
    Fix this by clearing napi->skb before the skb is freed.
    
    Fixes: 9b8dd5e5ea48 ("gve: DQO: Add RX path")
    Cc: stable@vger.kernel.org
    Reported-by: Shailend Chand <shailend@google.com>
    Signed-off-by: Ziwei Xiao <ziweixiao@google.com>
    Reviewed-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Reviewed-by: Shailend Chand <shailend@google.com>
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Link: https://lore.kernel.org/r/20240612001654.923887-1-ziweixiao@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 6f4d93b78ade0a4c2cafd587f7b429ce95abb02e

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.2-3-8633c55-d00dc15-oot"
+#define GVE_VERSION		 "1.4.2-5-8633c55-e1255ec-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
