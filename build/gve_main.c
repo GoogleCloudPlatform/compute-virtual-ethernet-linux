69aed82aefede3d5a4404a19016e36e766858867
Jeroen de Borst <jeroendb@google.com>
2024-06-25 00:12:28 +0000

    gve: Add adminq extended command
    The adminq command is limited to 64 bytes per entry and it's 56 bytes
    for the command itself at maximum. To support larger commands, we need
    to dma_alloc a separate memory to put the command in that memory and
    send the dma memory address instead of the actual command.
    
    Introduce an extended adminq command to wrap the real command with the
    inner opcode and the allocated dma memory address specified. Once the
    device receives it, it can get the real command from the given dma
    memory address. As designed with the device, all the extended commands
    will use inner opcode larger than 0xFF.
    
    Signed-off-by: Jeroen de Borst <jeroendb@google.com>
    Co-developed-by: Ziwei Xiao <ziweixiao@google.com>
    Signed-off-by: Ziwei Xiao <ziweixiao@google.com>
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Reviewed-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Link: https://patch.msgid.link/20240625001232.1476315-3-ziweixiao@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: fcfe6318dbac45e6c3c5c7a40f5dd5c08600a5e9

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.2-7-8633c55-825feca-oot"
+#define GVE_VERSION		 "1.4.2-9-8633c55-0d2caa3-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
