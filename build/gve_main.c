ab6e58ae6cde6d409ffa831cc362fa8aa0f66cae
Ziwei Xiao <ziweixiao@google.com>
2024-09-17 22:33:55 +0000

    gve: Fix header split Coccinelle patch for using wrong size
    When checking if max rx buffer size is supported, it wrongly compares
    with the GVE_DEFAULT_HEADER_BUFFER_SIZE. Instead, it should compare with
    GVE_DEFAULT_RX_BUFFER_SIZE.

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.3-0--1f1e902-oot"
+#define GVE_VERSION		 "1.4.3-0--7bbab4e-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
