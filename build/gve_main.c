e1b442bc7e672dd8837e10f356a8adbbd459f072
Joe Damato <jdamato@fastly.com>
2024-09-30 21:07:07 +0000

    gve: Map IRQs to NAPI instances
    Use netdev-genl interface to map IRQs to NAPI instances so that this
    information is accessible by user apps via netlink.
    
    $ cat /proc/interrupts | grep gve | grep -v mgmnt | cut -f1 --delimiter=':'
     34
     35
     36
     37
     38
     39
     40
    [...]
     65
    
    $ ./tools/net/ynl/cli.py --spec Documentation/netlink/specs/netdev.yaml \
                             --dump napi-get --json='{"ifindex": 2}'
    [{'id': 8288, 'ifindex': 2, 'irq': 65},
      [...]
     {'id': 8263, 'ifindex': 2, 'irq': 40},
     {'id': 8262, 'ifindex': 2, 'irq': 39},
     {'id': 8261, 'ifindex': 2, 'irq': 38},
     {'id': 8260, 'ifindex': 2, 'irq': 37},
     {'id': 8259, 'ifindex': 2, 'irq': 36},
     {'id': 8258, 'ifindex': 2, 'irq': 35},
     {'id': 8257, 'ifindex': 2, 'irq': 34}]
    
    Signed-off-by: Joe Damato <jdamato@fastly.com>
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Link: https://patch.msgid.link/20240930210731.1629-2-jdamato@fastly.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 3017238b60d394d01f2596e07343ca494b6dd2c0

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.4-0--e9c53c4-oot"
+#define GVE_VERSION		 "1.4.4-0--f43bfd8-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
