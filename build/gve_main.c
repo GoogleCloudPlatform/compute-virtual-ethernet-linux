c4830b31008fcc0d0a0a9c59a9b169242ba06453
Ziwei Xiao <ziweixiao@google.com>
2024-11-13 09:59:30 -0800

    gve: Flow steering trigger reset only for timeout error
    When configuring flow steering rules, the driver is currently going
    through a reset for all errors from the device. Instead, the driver
    should only reset when there's a timeout error from the device.
    
    Fixes: 57718b60df9b ("gve: Add flow steering adminq commands")
    Cc: stable@vger.kernel.org
    Signed-off-by: Ziwei Xiao <ziweixiao@google.com>
    Signed-off-by: Jeroen de Borst <jeroendb@google.com>
    Reviewed-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20241113175930.2585680-1-jeroendb@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 8ffade77b6337a8767fae9820d57d7a6413dd1a1

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.4-0--6ab5391-oot"
+#define GVE_VERSION		 "1.4.4-0--0a90bc3-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
