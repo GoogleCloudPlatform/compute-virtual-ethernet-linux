57da73939482b4f4293b9f8d37207008883b3033
Dr. David Alan Gilbert <linux@treblig.org>
2024-12-11 00:19:27 +0000

    gve: Remove unused gve_adminq_set_mtu
    The last use of gve_adminq_set_mtu() was removed by
    commit 37149e9374bf ("gve: Implement packet continuation for RX.")
    
    Remove it.
    
    Signed-off-by: Dr. David Alan Gilbert <linux@treblig.org>
    Reviewed-by: Kalesh AP <kalesh-anakkur.purayil@broadcom.com>
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Link: https://patch.msgid.link/20241211001927.253161-1-linux@treblig.org
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5.1-0-d3ec7a0-d3ec7a0-oot"
+#define GVE_VERSION		 "1.4.5.1-5-8578b2d-44e8815-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
