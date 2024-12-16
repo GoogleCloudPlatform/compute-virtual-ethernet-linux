82f9b4b36064db1bc8319a2386b9c70de76e2c84
Ziwei Xiao <ziweixiao@google.com>
2024-12-16 19:11:52 +0000

    Bump Version to 1.4.5

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.4-0--76f4d94-oot"
+#define GVE_VERSION		 "1.4.5-0--b95625d-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
