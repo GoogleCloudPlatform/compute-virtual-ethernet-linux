939f5355c66e8fa7cccf3ccff22e934fee8a0879
Joshua Washington <joshwash@google.com>
2024-12-18 05:34:12 -0800

    gve: guard XDP xmit NDO on existence of xdp queues
    In GVE, dedicated XDP queues only exist when an XDP program is installed
    and the interface is up. As such, the NDO XDP XMIT callback should
    return early if either of these conditions are false.
    
    In the case of no loaded XDP program, priv->num_xdp_queues=0 which can
    cause a divide-by-zero error, and in the case of interface down,
    num_xdp_queues remains untouched to persist XDP queue count for the next
    interface up, but the TX pointer itself would be NULL.
    
    The XDP xmit callback also needs to synchronize with a device
    transitioning from open to close. This synchronization will happen via
    the GVE_PRIV_FLAGS_NAPI_ENABLED bit along with a synchronize_net() call,
    which waits for any RCU critical sections at call-time to complete.
    
    Fixes: 39a7f4aa3e4a ("gve: Add XDP REDIRECT support for GQI-QPL format")
    Cc: stable@vger.kernel.org
    Signed-off-by: Joshua Washington <joshwash@google.com>
    Signed-off-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Reviewed-by: Shailend Chand <shailend@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    net-next: ff7c2dea9dd1a436fc79d6273adffdcc4a7ffea3

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5.1-6-8578b2d-a012d11-oot"
+#define GVE_VERSION		 "1.4.5.1-7-8578b2d-b77b497-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
@@ -2148,6 +2148,9 @@ static void gve_turndown(struct gve_priv *priv)
 
 	gve_clear_napi_enabled(priv);
 	gve_clear_report_stats(priv);
+
+	/* Make sure that all traffic is finished processing. */
+	synchronize_net();
 }
 
 static void gve_turnup(struct gve_priv *priv)
