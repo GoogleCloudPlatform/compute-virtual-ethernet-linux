93f8081ec9af4de95223c8c83f37733aca063987
Harshitha Ramamurthy <hramamurthy@google.com>
2025-03-17 21:41:41 +0000

    gve: unlink old napi only if page pool exists
    Commit de70981f295e ("gve: unlink old napi when stopping a queue using
    queue API") unlinks the old napi when stopping a queue. But this breaks
    QPL mode of the driver which does not use page pool. Fix this by checking
    that there's a page pool associated with the ring.
    
    Cc: stable@vger.kernel.org
    Fixes: de70981f295e ("gve: unlink old napi when stopping a queue using queue API")
    Reviewed-by: Joshua Washington <joshwash@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20250317214141.286854-1-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 81273eb87af86d4a43244b553762348e364b2df7

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5-0--074512c-oot"
+#define GVE_VERSION		 "1.4.5-0--ee6a85f-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
