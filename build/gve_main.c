90d49c4812e0ce65a7ee847ac9390dce28a8e557
Joshua Washington <joshwash@google.com>
2025-03-21 00:29:05 +0000

    gve: remove xdp_xsk_done and xdp_xsk_wakeup statistics
    These statistics pollute the hotpath and do not have any real-world use
    or meaning.
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Joshua Washington <joshwash@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Link: https://patch.msgid.link/20250321002910.1343422-2-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: c2b900958535a6b958f86245e9afca2df59329bd

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5-0--8db2cc2-oot"
+#define GVE_VERSION		 "1.4.5-0--a639c77-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
