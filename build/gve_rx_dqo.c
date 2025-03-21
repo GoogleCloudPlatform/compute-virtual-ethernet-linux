2f9189edf78201ebcba9d7f11950e22f25f1bf49
Joshua Washington <joshwash@google.com>
2025-03-21 00:29:06 +0000

    gve: introduce config-based allocation for XDP
    An earlier patch series[1] introduced RX/TX ring allocation configuration
    structs which contained metadata used to allocate and configure new RX
    and TX rings. This led to a much cleaner and safer allocation pattern
    wherein queue resources were not deallocated until new queue resources
    were successfully allocated.
    
    Migrate the XDP allocation path to use the same pattern to allow for the
    existence of a single allocation path instead of relying on XDP-specific
    allocation methods. These extra allocation methods result in the
    duplication of many existing behaviors while being prone to error when
    configuration changes unrelated to XDP occur.
    
    Link: https://lore.kernel.org/netdev/20240122182632.1102721-1-shailend@google.com/ [1]
    Reviewed-by: Praveen Kaligineedi <pkaligineedi@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Joshua Washington <joshwash@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Link: https://patch.msgid.link/20250321002910.1343422-3-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 542a58f1b090cdcea951b7c9c48c3ba57d1e35b1

--- build/gve_rx_dqo.c
+++ build/gve_rx_dqo.c
@@ -348,16 +348,16 @@ int gve_rx_alloc_rings_dqo(struct gve_priv *priv,
 	int i;
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0)
-	rx = kvcalloc(cfg->qcfg->max_queues, sizeof(struct gve_rx_ring),
+	rx = kvcalloc(cfg->qcfg_rx->max_queues, sizeof(struct gve_rx_ring),
 		      GFP_KERNEL);
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0) */
-	rx = kcalloc(cfg->qcfg->max_queues, sizeof(struct gve_rx_ring),
+	rx = kcalloc(cfg->qcfg_rx->max_queues, sizeof(struct gve_rx_ring),
 		     GFP_KERNEL);
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0) */
 	if (!rx)
 		return -ENOMEM;
 
-	for (i = 0; i < cfg->qcfg->num_queues; i++) {
+	for (i = 0; i < cfg->qcfg_rx->num_queues; i++) {
 		err = gve_rx_alloc_ring_dqo(priv, cfg, &rx[i], i);
 		if (err) {
 			netif_err(priv, drv, priv->dev,
@@ -390,7 +390,7 @@ void gve_rx_free_rings_dqo(struct gve_priv *priv,
 	if (!rx)
 		return;
 
-	for (i = 0; i < cfg->qcfg->num_queues;  i++)
+	for (i = 0; i < cfg->qcfg_rx->num_queues;  i++)
 		gve_rx_free_ring_dqo(priv, &rx[i], cfg);
 
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4,18,0)
