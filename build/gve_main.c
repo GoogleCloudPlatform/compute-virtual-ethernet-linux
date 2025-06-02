90fa95d09387cba96c2cab3383e9243740ba46d9
Alok Tiwari <alok.a.tiwari@oracle.com>
2025-06-02 03:34:29 -0700

    gve: add missing NULL check for gve_alloc_pending_packet() in TX DQO
    gve_alloc_pending_packet() can return NULL, but gve_tx_add_skb_dqo()
    did not check for this case before dereferencing the returned pointer.
    
    Add a missing NULL check to prevent a potential NULL pointer
    dereference when allocation fails.
    
    This improves robustness in low-memory scenarios.
    
    Fixes: a57e5de476be ("gve: DQO: Add TX path")
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Reviewed-by: Mina Almasry <almasrymina@google.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    net-next: 12c331b29c7397ac3b03584e12902990693bc248

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5.1-34-8578b2d-835c474-oot"
+#define GVE_VERSION		 "1.4.5.1-35-8578b2d-cded598-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
