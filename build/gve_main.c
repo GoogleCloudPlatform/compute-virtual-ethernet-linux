18688ae1a946ff1aee016731c67a5f8fa2c9eee6
John Fraker <jfraker@google.com>
2025-06-14 00:07:52 +0000

    gve: Add rx hardware timestamp expansion
    Allow the rx path to recover the high 32 bits of the full 64 bit rx
    timestamp.
    
    Use the low 32 bits of the last synced nic time and the 32 bits of the
    timestamp provided in the rx descriptor to generate a difference, which
    is then applied to the last synced nic time to reconstruct the complete
    64-bit timestamp.
    
    This scheme remains accurate as long as no more than ~2 seconds have
    passed between the last read of the nic clock and the timestamping
    application of the received packet.
    
    Signed-off-by: John Fraker <jfraker@google.com>
    Signed-off-by: Ziwei Xiao <ziweixiao@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Link: https://patch.msgid.link/20250614000754.164827-7-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 3bf5431fef750d5081731b53f2a5c324bcff6c3a

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5.1-42-8578b2d-7e368c1-oot"
+#define GVE_VERSION		 "1.4.5.1-43-8578b2d-7d12c9e-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
