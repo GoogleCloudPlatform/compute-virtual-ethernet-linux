7022c96115c9518c40cb7df57fd3beb77861b550
John Fraker <jfraker@google.com>
2025-06-14 00:07:54 +0000

    gve: Advertise support for rx hardware timestamping
    Expand the get_ts_info ethtool handler with the new gve_get_ts_info
    which advertises support for rx hardware timestamping.
    
    With this patch, the driver now fully supports rx hardware timestamping.
    
    Signed-off-by: John Fraker <jfraker@google.com>
    Signed-off-by: Ziwei Xiao <ziweixiao@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Link: https://patch.msgid.link/20250614000754.164827-9-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: a471e7f87e08d94bcdb013482551abfc455340e7

--- build/gve_main.c
+++ build/gve_main.c
@@ -38,7 +38,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5.1-44-8578b2d-3cc6331-oot"
+#define GVE_VERSION		 "1.4.5.1-45-8578b2d-4113ab9-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
