ea2a2f515115c3f6ab86f5a141a1f20667688386
Alok Tiwari <alok.a.tiwari@oracle.com>
2025-06-15 22:45:00 -0700

    gve: Fix various typos and improve code comments
    - Correct spelling and improves the clarity of comments
       "confiugration" -> "configuration"
       "spilt" -> "split"
       "It if is 0" -> "If it is 0"
       "DQ" -> "DQO" (correct abbreviation)
    - Clarify BIT(0) flag usage in gve_get_priv_flags()
    - Replaced hardcoded array size with GVE_NUM_PTYPES
      for clarity and maintainability.
    
    These changes are purely cosmetic and do not affect functionality.
    
    Signed-off-by: Alok Tiwari <alok.a.tiwari@oracle.com>
    Reviewed-by: Joe Damato <joe@dama.to>
    Reviewed-by: Mina Almasry <almasrymina@google.com>
    Link: https://patch.msgid.link/20250616054504.1644770-1-alok.a.tiwari@oracle.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: b52a93bbaa51a3f03561fd37221af29655db5c2a

--- build/gve_ethtool.c
+++ build/gve_ethtool.c
@@ -762,7 +762,7 @@ static u32 gve_get_priv_flags(struct net_device *netdev)
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,8,0))
 	u32 ret_flags = 0;
 
-	/* Only 1 flag exists currently: report-stats (BIT(O)), so set that flag. */
+	/* Only 1 flag exists currently: report-stats (BIT(0)), so set that flag. */
 	if (priv->ethtool_flags & BIT(0))
 		ret_flags |= BIT(0);
 	return ret_flags;
