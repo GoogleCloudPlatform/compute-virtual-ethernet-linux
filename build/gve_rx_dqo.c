3a9311d46bf97f8ee6dffe69b6267180228d7556
Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
2025-07-03 20:21:20 +0200

    gve: global: fix "for a while" typo
    Signed-off-by: Ahelenia Ziemiańska <nabijaczleweli@nabijaczleweli.xyz>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/5zsbhtyox3cvbntuvhigsn42uooescbvdhrat6s3d6rczznzg5@tarta.nabijaczleweli.xyz
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: f142028e30ca96cd0f490be7af54a60f59de7a45

--- build/gve_rx_dqo.c
+++ build/gve_rx_dqo.c
@@ -807,7 +807,7 @@ static int gve_rx_dqo(struct napi_struct *napi, struct gve_rx_ring *rx,
 	buf_len = compl_desc->packet_len;
 	hdr_len = compl_desc->header_len;
 
-	/* Page might have not been used for awhile and was likely last written
+	/* Page might have not been used for a while and was likely last written
 	 * by a different thread.
 	 */
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,14,0))
