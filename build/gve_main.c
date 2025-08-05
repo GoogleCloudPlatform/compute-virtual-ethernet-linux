8e6488d932f239db23657b39bc8087ba4e8edd96
Ziwei Xiao <ziweixiao@google.com>
2025-08-05 17:37:41 +0000

    Bumping Version to 1.4.6

--- build/gve_main.c
+++ build/gve_main.c
@@ -39,7 +39,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.5.1-1-388d2c9-fc517b1-oot"
+#define GVE_VERSION		 "1.4.6-1-388d2c9-56949b3-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
