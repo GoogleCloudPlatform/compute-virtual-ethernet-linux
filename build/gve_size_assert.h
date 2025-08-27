3308a0fecc0b4b751ff28af88084035f0b6e98a5
israel4435-beep <israel4435@gmail.com>
2025-08-27 12:38:35 -0700

    Delete build directory

--- build/gve_size_assert.h
+++
@@ -1,9 +0,0 @@
-/* SPDX-License-Identifier: (GPL-2.0 OR MIT)
- * Google virtual Ethernet (gve) driver
- *
- * Copyright (C) 2015-2019 Google, Inc.
- */
-
-#ifndef static_assert
-#define static_assert(expr, ...) _Static_assert(expr, #expr)
-#endif /* static_assert */
