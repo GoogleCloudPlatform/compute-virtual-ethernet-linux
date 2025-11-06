9cca387a4fd416c59697add18b9b87997045ff6f
Ankit Garg <nktgrg@google.com>
2025-11-06 11:27:46 -0800

    gve: Default to max_rx_buffer_size for DQO if device supported
    Change the driver's default behavior to prefer the largest available RX
    buffer length supported by the device for DQO format, rather than always
    using the hardcoded 2K default.
    
    Previously, the driver would initialize with
    `GVE_DEFAULT_RX_BUFFER_SIZE` (2K), even if the device advertised support
    for a larger length (e.g., 4K).
    
    Performance observations:
    - With LRO disabled, we observed >10% improvement in RX single stream
    throughput when MTU >=2048.
    - With LRO enabled, we observed >10% improvement in RX single stream
    throughput when MTU >=1460.
    - No regressions were observed.
    
    Signed-off-by: Ankit Garg <nktgrg@google.com>
    Reviewed-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Reviewed-by: Jordan Rhee <jordanrhee@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Joshua Washington <joshwash@google.com>
    Link: https://patch.msgid.link/20251106192746.243525-5-joshwash@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 09a81a0f4fb7346f80a7340f725aedccff8ef8e4

--- build/gve_main.c
+++ build/gve_main.c
@@ -39,7 +39,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.7-0--197a1fa-oot"
+#define GVE_VERSION		 "1.4.7-0--71ceaf8-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
