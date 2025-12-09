60a2dc812afe0efc3d1b4456f49c80b0ee3b4c78
Ankit Garg <nktgrg@google.com>
2025-12-09 02:08:07 +0000

    Bump version to 1.4.8

--- google/gve/gve_main.c
+++ google/gve/gve_main.c
@@ -30,7 +30,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		"1.4.7"
+#define GVE_VERSION		"1.4.8"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
