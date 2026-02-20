ce056a984f1cf386b3833e657f2db19f3c190155
Ankit Garg <nktgrg@google.com>
2026-02-20 13:53:24 -0800

    gve: fix incorrect buffer cleanup in gve_tx_clean_pending_packets for QPL
    In DQ-QPL mode, gve_tx_clean_pending_packets() incorrectly uses the RDA
    buffer cleanup path. It iterates num_bufs times and attempts to unmap
    entries in the dma array.
    
    This leads to two issues:
    1. The dma array shares storage with tx_qpl_buf_ids (union).
     Interpreting buffer IDs as DMA addresses results in attempting to
     unmap incorrect memory locations.
    2. num_bufs in QPL mode (counting 2K chunks) can significantly exceed
     the size of the dma array, causing out-of-bounds access warnings
    (trace below is how we noticed this issue).
    
    UBSAN: array-index-out-of-bounds in
    drivers/net/ethernet/google/gve/gve_tx_dqo.c:178:5 index 18 is out of
    range for type 'dma_addr_t[18]' (aka 'unsigned long long[18]')
    Workqueue: gve gve_service_task [gve]
    Call Trace:
    <TASK>
    dump_stack_lvl+0x33/0xa0
    __ubsan_handle_out_of_bounds+0xdc/0x110
    gve_tx_stop_ring_dqo+0x182/0x200 [gve]
    gve_close+0x1be/0x450 [gve]
    gve_reset+0x99/0x120 [gve]
    gve_service_task+0x61/0x100 [gve]
    process_scheduled_works+0x1e9/0x380
    
    Fix this by properly checking for QPL mode and delegating to
    gve_free_tx_qpl_bufs() to reclaim the buffers.
    
    Cc: stable@vger.kernel.org
    Fixes: a6fb8d5a8b69 ("gve: Tx path for DQO-QPL")
    Signed-off-by: Ankit Garg <nktgrg@google.com>
    Reviewed-by: Jordan Rhee <jordanrhee@google.com>
    Reviewed-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Signed-off-by: Joshua Washington <joshwash@google.com>
    Reviewed-by: Simon Horman <horms@kernel.org>
    Link: https://patch.msgid.link/20260220215324.1631350-1-joshwash@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: fb868db5f4bccd7a78219313ab2917429f715cea

--- build/gve_main.c
+++ build/gve_main.c
@@ -39,7 +39,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.9-4-328e95fda549-5ceacddc0f27-oot"
+#define GVE_VERSION		 "1.4.9-5-328e95fda549-6cee403085b1-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
