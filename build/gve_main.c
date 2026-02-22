ac4e49a238ec30dcfaccb73ca17e75835ad10851
Linus Torvalds <torvalds@linux-foundation.org>
2026-02-21 20:03:00 -0800

    Convert more 'alloc_obj' cases to default GFP_KERNEL arguments
    This converts some of the visually simpler cases that have been split
    over multiple lines.  I only did the ones that are easy to verify the
    resulting diff by having just that final GFP_KERNEL argument on the next
    line.
    
    Somebody should probably do a proper coccinelle script for this, but for
    me the trivial script actually resulted in an assertion failure in the
    middle of the script.  I probably had made it a bit _too_ trivial.
    
    So after fighting that far a while I decided to just do some of the
    syntactically simpler cases with variations of the previous 'sed'
    scripts.
    
    The more syntactically complex multi-line cases would mostly really want
    whitespace cleanup anyway.
    
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    net-next: 32a92f8c89326985e05dce8b22d3f0aa07a3e1bd

--- build/gve_main.c
+++ build/gve_main.c
@@ -39,7 +39,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.9-2-328e95fda549-3a2f743f6599-oot"
+#define GVE_VERSION		 "1.4.9-3-328e95fda549-ce4aa9c090f1-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
@@ -833,8 +833,7 @@ static int gve_setup_device_resources(struct gve_priv *priv)
 	}
 
 	if (!gve_is_gqi(priv)) {
-		priv->ptype_lut_dqo = kvzalloc_obj(*priv->ptype_lut_dqo,
-						   GFP_KERNEL);
+		priv->ptype_lut_dqo = kvzalloc_obj(*priv->ptype_lut_dqo);
 		if (!priv->ptype_lut_dqo) {
 			err = -ENOMEM;
 			goto abort_with_stats_report;
