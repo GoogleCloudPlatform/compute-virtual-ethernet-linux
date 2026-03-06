8d54e55eb6f360b48e65c1e2e9dd3087aed1d9ec
Ankit Garg <nktgrg@google.com>
2026-03-06 22:48:16 +0000

    gve: add support for UDP GSO for DQO format
    Enable support for UDP GSO when using DQO format. Advertise the feature
    flag during device initialization and enable offload by default.
    
    Signed-off-by: Ankit Garg <nktgrg@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Link: https://patch.msgid.link/20260306224816.3391551-1-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 014c607f86abc903d7bf46e13373d89392e371fe

--- build/gve_main.c
+++ build/gve_main.c
@@ -40,7 +40,7 @@
 #define GVE_DEFAULT_RX_COPYBREAK	(256)
 
 #define DEFAULT_MSG_LEVEL	(NETIF_MSG_DRV | NETIF_MSG_LINK)
-#define GVE_VERSION		 "1.4.9-11-328e95fda549-1e428929a343-oot"
+#define GVE_VERSION		 "1.4.9-12-328e95fda549-7067cc2b3b9a-oot"
 #define GVE_VERSION_PREFIX	"GVE-"
 
 // Minimum amount of time between queue kicks in msec (10 seconds)
