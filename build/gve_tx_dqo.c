9882cf6c25f6a9cadf5cd6ee612835848e538299
Ankit Garg <nktgrg@google.com>
2026-03-06 22:48:16 +0000

    gve: add support for UDP GSO for DQO format
    Enable support for UDP GSO when using DQO format. Advertise the feature
    flag during device initialization and enable offload by default.
    
    Signed-off-by: Ankit Garg <nktgrg@google.com>
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Signed-off-by: Harshitha Ramamurthy <hramamurthy@google.com>
    Link: https://patch.msgid.link/20260306224816.3391551-1-hramamurthy@google.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>
    net-next: 014c607f86abc903d7bf46e13373d89392e371fe

--- build/gve_tx_dqo.c
+++ build/gve_tx_dqo.c
@@ -614,9 +614,11 @@ static void gve_tx_fill_pkt_desc_dqo(struct gve_tx_ring *tx, u32 *desc_idx,
  */
 static int gve_prep_tso(struct sk_buff *skb)
 {
+	struct skb_shared_info *shinfo = skb_shinfo(skb);
+	u32 paylen, l4_start;
 	struct tcphdr *tcp;
+	struct udphdr *udp;
 	int header_len;
-	u32 paylen;
 	int err;
 
 	/* Note: HW requires MSS (gso_size) to be <= 9728 and the total length
@@ -627,29 +629,46 @@ static int gve_prep_tso(struct sk_buff *skb)
 	 * - Kernel will not produce a TSO larger than 64k
 	 */
 
-	if (unlikely(skb_shinfo(skb)->gso_size < GVE_TX_MIN_TSO_MSS_DQO))
+	if (unlikely(shinfo->gso_size < GVE_TX_MIN_TSO_MSS_DQO))
 		return -1;
-
-	if (!(skb_shinfo(skb)->gso_type & (SKB_GSO_TCPV4 | SKB_GSO_TCPV6)))
-		return -EINVAL;
 
 	/* Needed because we will modify header. */
 	err = skb_cow_head(skb, 0);
 	if (err < 0)
 		return err;
 
-	tcp = tcp_hdr(skb);
-	paylen = skb->len - skb_transport_offset(skb);
+	l4_start = skb_transport_offset(skb);
+	paylen = skb->len - l4_start;
+
+	switch (shinfo->gso_type) {
+	case SKB_GSO_TCPV4:
+	case SKB_GSO_TCPV6:
+		tcp = tcp_hdr(skb);
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0))
-	csum_replace_by_diff(&tcp->check, (__force __wsum)htonl(paylen));
+		csum_replace_by_diff(&tcp->check,
+				     (__force __wsum)htonl(paylen));
 #else /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) */
-	tcp->check = csum_fold(csum_add((__force __wsum)htonl(paylen), ~csum_unfold(tcp->check)));
+		tcp->check = csum_fold(csum_add((__force __wsum)htonl(paylen), ~csum_unfold(tcp->check)));
 #endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) */
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0))
-	header_len = skb_tcp_all_headers(skb);
+		header_len = skb_tcp_all_headers(skb);
 #else
-	header_len = skb_transport_offset(skb) + tcp_hdrlen(skb);
+		header_len = skb_transport_offset(skb) + tcp_hdrlen(skb);
 #endif
+		break;
+	case SKB_GSO_UDP_L4:
+		udp = udp_hdr(skb);
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0))
+		csum_replace_by_diff(&udp->check,
+				     (__force __wsum)htonl(paylen));
+#else /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) */
+		udp->check = csum_fold(csum_add((__force __wsum)htonl(paylen), ~csum_unfold(udp->check)));
+#endif /* LINUX_VERSION_CODE >= KERNEL_VERSION(4,6,0) */
+		header_len = sizeof(struct udphdr) + l4_start;
+		break;
+	default:
+		return -EINVAL;
+	}
 
 	if (unlikely(header_len > GVE_TX_MAX_HDR_SIZE_DQO))
 		return -EINVAL;
